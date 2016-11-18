ce5642920e03ae220284d76662ddd1a618cff40a
Ryan Running <ryanrunning@securitymetrics.com>
2016-11-17 19:15:32 -0700

    Adds styling, funtionality and some buttons

--- calculator.js
+++ calculator.js
@@ -10,8 +10,16 @@ $(document).ready (function() {
 	$('#clear').on('click', clearField);	
 
 	function insert() {
-			var input = $('#display-field'); 
-			input.html(input.html() + $(this).html())
+			var input = $('#display-field');
+			var el = $(this);
+			if(operatorAllowed === false & el.hasClass('operator')) {
+				alert("Invalid sequence!");
+				return;
+			} 
+			input.html(input.html() + $(this).html());
+			allowOperator(el);
+			console.log(operatorAllowed);
+			// addOperator();
 	}
 
 	function math() {
@@ -21,7 +29,29 @@ $(document).ready (function() {
 	}
 
 	function clearField() {
+		recentEntries.push($('#display-field').html());
+		lastFiveEntries();
 		$('#display-field').html('');
+		console.log(recentEntries);
+	}
+	function addOperator() {
+		var displayString = $('#display-field').html();
+		var lastCharacter = displayString.charAt(displayString.length - 1);
+		console.log(lastCharacter);
 	}
 });
+var recentEntries = []
+function lastFiveEntries() {
+	$('ul').append($('<li>').text(recentEntries[recentEntries.length - 1]));
+}
 
+// Do not allow any operators to be placed side by side.
+var operatorAllowed = true;
+
+function allowOperator(peanut) {
+	if(peanut.hasClass('operator')) {
+		operatorAllowed = false;
+	} else {
+		operatorAllowed = true;
+	}
+}
